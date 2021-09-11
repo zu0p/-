432d641cd846bcce9a33db5f0305f7dd57290bdd
Seungyeup <lsyeup1206@naver.com>
2021-09-11 11:00:00 +0900

    user login control

--- backend/crud/user_crud.py
+++ backend/crud/user_crud.py
@@ -1,27 +1,63 @@
 
 
+from typing import Optional
+from backend.schemas.user_schemas import UserInDB
 from database import SessionLocal
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 from models import user_model
 from schemas import user_schemas
+from sqlalchemy.orm import Session
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 # ============================ user Auth ============================
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = "a6480a82721880f4cebcd01f4963f65e126fdf0c15292b3817cac34c7198bad5"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# 비밀번호 확인
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+# 비밀번호 hash
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+# access-token 발급
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# 유저 반환
+def get_user(db: SessionLocal, userId: str):
+    return db.query(user_model.UserInfo).filter(user_model.UserInfo.userId == userId).first()
+
+# 유저 확인
+def authenticate_user(SessionLocal, username: str, password: str):
+    user = get_user(SessionLocal, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
 # ============================ user CRUD ============================
+
 def create_user(db: SessionLocal, user_data: user_schemas.UserInDB):
     db_user = user_model.UserInfo(
         userId = user_data.userId,
--- backend/views/user_views.py
+++ backend/views/user_views.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, HTTPException, Depends
+from datetime import datetime, timedelta
+from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 from database import get_db
 
 from schemas import user_schemas # schemas
@@ -8,18 +11,27 @@ from crud import user_crud # crud
 
 user_router = APIRouter()
 
-# user Auth
-@user_router.post("/login")
-def login(login_data: user_schemas.UserLoginReq):
-    print(login_data)
-    return {"access_token": login_data.userId, "token_type": login_data.userPwd}
+Session = Depends(get_db)
+
+# ================================== user Auth ==================================
+@user_router.post("/login", response_model=user_schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = user_crud.authenticate_user(Session, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=user_crud.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = user_crud.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
-
-
-# user CRUD
-
+# ================================== user CRUD ==================================
 @user_router.post("/signup", response_model=user_schemas.UserInDB)
-def sign_up(user_data: user_schemas.UserInDB, db: Session = Depends(get_db)):
+def sign_up(user_data: user_schemas.UserInDB, db: Session):
     new_user = user_crud.create_user(db, user_data)
     return new_user
