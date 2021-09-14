b96a0c6ff06ace787f571ed750e6dc4b288ffab9
Jihoon Byun <wlgns1588@naver.com>
2021-09-14 19:47:49 +0900

    chore: 이미지 크롤러 동적 페이지 되게 변경

--- crawler/image_crawler.py
+++ crawler/image_crawler.py
@@ -10,24 +10,55 @@ from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 from urllib.parse import quote_plus
 
 
-def main():
-    keyword = 'dad'
-    url = 'https://unsplash.com/s/photos/' + keyword
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    images = soup.find_all(class_='oCCRx', limit=30)  # 클래스가 oCCRx인 사진 20장
-    print(images)
+def image_crawling(keyword):
+    driver = driver_init()
 
+    url = 'https://unsplash.com/s/photos/' + keyword
+    # 페이지 로드를 위해 3초 대기
+    driver.implicitly_wait(3)
+    driver.get(url)
+
+    elem = driver.find_element_by_tag_name("body")
+    for i in range(10):
+        elem.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.3)
+
+    images = driver.find_elements_by_class_name("oCCRx")
+    print(len(images))
     n = 1
     for image in images:
-        img_urls = image['srcset'].split()
+        img_urls = image.get_attribute('srcset')
+
+        if img_urls is None:
+            continue
+
+        img_urls = img_urls.split()
+
+        if len(img_urls) < 1:
+            continue
+
         img_url = img_urls[20]
-        print(img_url)
+
         with urlopen(img_url) as f:
-            with open('./img/' + keyword + str(n) + '.jpg', 'wb') as h:  # 이미지 + 사진번호 .jpg
+            with open('./images/' + keyword + str(n) + '.jpg', 'wb') as h:  # 이미지 + 사진번호 .jpg
                 img = f.read()  # 이미지 읽기
                 h.write(img)
         n += 1
+        if n > 20:
+            break
+
+
+def driver_init():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(
+        chrome_options=options,  executable_path=r'C:/SSAFY/2학기/특화PJT/crawler/chromedriver.exe')
+
+    return driver
+
+
+def main():
+    image_crawling("mom")
 
 
 if __name__ == '__main__':
