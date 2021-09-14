62913959bb88ecd05ba80f515f0007ed87004671
multicampus <sun950903@gmail.com>
2021-09-14 18:14:36 +0900

    Feat: 라벨링 엑셀 데이터 추가

--- ai/emotion_label.py
+++ ai/emotion_label.py
@@ -15,6 +15,7 @@ from konlpy.tag import Okt
 excel_file = openpyxl.Workbook()
 excel_sheet = excel_file.active
 excel_sheet.append(["content", "emotion"])
+word_list = []
 
 
 class List(list):
@@ -31,24 +32,101 @@ def test3():
 
 def changeSentence():
     # write_data = pd.read_excel('article.xlsx')
-    write_data = pd.read_excel('article2.xlsx', engine='openpyxl')
+    write_data = pd.read_excel('article.xlsx', engine='openpyxl')
+    emotion_dir = pd.read_excel('emotion_directory.xlsx', engine='openpyxl')
+    emotion_dir = emotion_dir.fillna(0)
+
     # print(chatbot_data)
-    print(write_data.columns)
+    # print(write_data.columns)
     okt = Okt()
     df1 = write_data['content']  # title 버리고 content만 취함
-
+    dir1 = emotion_dir[['단어', '감정범주']]
+    list_dir1 = dir1.values.tolist()
+    # check = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    check = []
+    cnt =0
+    emotion = ['혐오', '기쁨', '슬픔', '중립', '기타', '분노', '흥미', '지루함', '놀람', '통증']
     for i in range(len(df1)):
-        temp = df1.loc[i]
-        for sent in kss.split_sentences(temp):
+        cnt = cnt+1
+        print(cnt)
+        stop_list = ["이다", "있다", "있어", "해서", "해도", "했지만", "하다", "한다", "했다"]
+        # print(len(df1))
+        temp = str(df1.loc[i])  # 글 하나씩 가져오기
+        for sent in kss.split_sentences(temp):  # 글에서 문장 하나씩 가져오기
+            word_list = []
+            check = []
+            check = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            word_lists = (List(okt.morphs(sent)))  # 문장에서 키워드 단어들 가져오기
+            for w in word_lists:  # 직접 정의한 불용어 처리
+                if w not in stop_list:
+                    word_list.append(w)
 
-            word_list = (List(okt.morphs(sent)))
+            for j in range(len(word_list)):  # 단어가 감정사전에 포함되 있는 단언지 확인하기
+                if type(word_list[j]) == type("test"):  # 숫자 들어올 때 거름
+                    if len(word_list[j]) <= 1:  # 1글자 짜리로 감정 예측하지 않기
+                        continue
+                    for u in range(len(list_dir1)):
+                        if(list_dir1[u][1] == 0):
+                            break
+                        if list_dir1[u][0].find(word_list[j]) != -1:
+                            if list_dir1[u][1] == '혐오':
+                                check[0] = int(check[0])+1
+                                break
 
-            print(word_list[1])
+                            elif list_dir1[u][1] == '기쁨':
+                                check[1] = int(check[1])+1
+                                break
 
-            # excel_sheet.append([sent, ""])
+                            elif list_dir1[u][1] == '슬픔':
+                                check[2] = int(check[2])+1
+                                break
 
-    # excel_file.save("sentence_emotion2.xlsx")
-    # excel_file.close
+                            elif list_dir1[u][1] == '중성':
+                                check[3] = check[3]+1
+                                break
+
+                            elif list_dir1[u][1] == '기타':
+                                check[4] = int(check[4])+1
+                                break
+
+                            elif list_dir1[u][1] == '분노':
+                                check[5] = int(check[5])+1
+                                break
+
+                            elif list_dir1[u][1] == '흥미':
+                                check[6] = int(check[6])+1
+                                break
+
+                            elif list_dir1[u][1] == '지루함':
+                                check[7] = check[7]+1
+                                break
+
+                            elif list_dir1[u][1] == '놀람':
+                                check[8] = int(check[8])+1
+                                break
+
+                            elif list_dir1[u][1] == '통증':
+                                check[9] = int(check[9])+1
+                                break
+
+            if max(check) == 0:
+                excel_sheet.append([sent, "중립"])
+            else:
+                idx = check.index(max(check))
+                if idx == 3 | idx == 4:
+                    check[3] = 0
+                    check[4] = 0
+                    idx = check.index(max(check))
+                    if max(check) == 0:
+                        excel_sheet.append([sent, "중립"])
+                    else:
+                        excel_sheet.append([sent, check[idx]])
+                else:
+                    excel_sheet.append([sent, emotion[idx]])
+
+            check = []
+        excel_file.save("sentence_emotion2.xlsx")
+        excel_file.close
 
 
 def main():
