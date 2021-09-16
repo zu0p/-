af20f623a3cd63d869bae0b264504a372fd9dc94
bwoo0822 <bwoo0822@naver.com>
2021-09-16 18:02:08 +0900

    feat: login update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,5 +10,10 @@ export default {
   name: 'App',
   components: { 
   },
+  data() {
+    return{
+      token: localStorage.getItem('jwt'),
+    }
+  }
 };
 </script>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,13 +3,13 @@
     <v-content class="back-img">
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8">
+          <v-col cols="12" sm="8" md="6">
             <v-card class="elevation-12">
               <v-window v-model="step">
                 <v-window-item :value="1">
                   <v-row>
                     <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
+                      <v-card-text class="mt-12 white--text">
                         <h1
                           class="text-center display-2 text--accent-3"
                         >그리더</h1>
@@ -17,43 +17,45 @@
                           class="text-center"
                         >:당신의 글에 이미지를 더하다</h5>
                         <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>fab fa-facebook-f</v-icon>
                           </v-btn>
 
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>fab fa-google-plus-g</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>fab fa-linkedin-in</v-icon>
                           </v-btn>
                         </div>
                         <h4 class="text-center mt-4">Ensure your ID for registration</h4>
                         <v-form>
                           <v-text-field
+                            dark
                             label="ID"
                             name="ID"
-                            prepend-icon="mdi-account-circle"
+                            prepend-icon="mdi-account-circle white--text"
                             type="text"
                             color="teal accent-3"
                           />
 
                           <v-text-field
-                            id="password"
+                            
                             label="Password"
                             name="password"
                             prepend-icon="lock"
                             type="password"
                             color="teal accent-3"
+                            dark
                           />
                         </v-form>
                         <h3 class="text-center mt-4">Forgot your password ?</h3>
                       </v-card-text>
                       <div class="text-center mt-3">
-                        <v-btn rounded color="accent-3" dark>SIGN IN</v-btn>
+                        <v-btn rounded color="grey accent-3" @click="clickLogin">SIGN IN</v-btn>
                       </div>
                     </v-col>
-                    <v-col cols="12" md="4" class="grey accent-3">
+                    <v-col cols="12" md="4" class="back-opt accent-3">
                       <v-card-text class="white--text mt-12">
                         <h1 class="text-center display-1">Welcome!!!</h1>
                         <h5
@@ -68,12 +70,12 @@
                 </v-window-item>
                 <v-window-item :value="2">
                   <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="grey accent-3">
+                    <v-col cols="12" md="4" class="back-opt accent-3">
                       <v-card-text class="white--text mt-12">
                         <h1 class="text-center display-1">Welcome Back!</h1>
                         <h5
                           class="text-center"
-                        >To Keep connected with us please login with your personnel info</h5>
+                        >일기쓰러 가기</h5>
                       </v-card-text>
                       <div class="text-center">
                         <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
@@ -81,28 +83,29 @@
                     </v-col>
 
                     <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
+                      <v-card-text class="mt-12 white--text">
                         <h1 class="text-center display-2 text--accent-3">그리더와 함께하기</h1>
                         <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>fab fa-facebook-f</v-icon>
                           </v-btn>
 
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>fab fa-google-plus-g</v-icon>
                           </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
+                          <v-btn class="mx-2" fab color="white" outlined>
                             <v-icon>fab fa-linkedin-in</v-icon>
                           </v-btn>
                         </div>
                         <h4 class="text-center mt-4">Ensure your email for registration</h4>
-                        <v-form>
+                        <v-form class="form-c">
                           <v-text-field
                             label="Name"
                             name="Name"
-                            prepend-icon="person"
+                            prepend-icon="mdi-account-details white--text"
                             type="text"
                             color="teal accent-3"
+                            dark
                           />
                           <v-text-field
                             label="Email"
@@ -110,6 +113,17 @@
                             prepend-icon="email"
                             type="text"
                             color="teal accent-3"
+                            dark
+                          />
+
+                          <v-text-field
+                            id="id"
+                            label="ID"
+                            name="id"
+                            prepend-icon="person"
+                            type="text"
+                            color="teal accent-3"
+                            dark
                           />
 
                           <v-text-field
@@ -119,11 +133,42 @@
                             prepend-icon="lock"
                             type="password"
                             color="teal accent-3"
+                            dark
+                          />
+
+                          <v-text-field
+                            id="nick"
+                            label="Nickname"
+                            name="nick"
+                            prepend-icon="mdi-alpha-n-circle white--text"
+                            type="text"
+                            color="teal accent-3"
+                            dark
+                          />
+
+                          <v-text-field
+                            id="phone"
+                            label="Phone Number"
+                            name="phone"
+                            prepend-icon="phone"
+                            type="text"
+                            color="teal accent-3"
+                            dark
+                          />
+
+                          <v-text-field
+                            id="userImage"
+                            label="User Image"
+                            name="userImage"
+                            prepend-icon="mdi-image-area white--text"
+                            type="text"
+                            color="teal accent-3"
+                            dark
                           />
                         </v-form>
                       </v-card-text>
                       <div class="text-center mt-n5">
-                        <v-btn rounded color="black accent-3" dark>SIGN UP</v-btn>
+                        <v-btn rounded color="grey accent-3">SIGN UP</v-btn>
                       </div>
                     </v-col>
                   </v-row>
@@ -139,12 +184,53 @@
 </template>
 
 <script>
+import {reactive, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import swal from 'sweetalert'
+
 export default {
+  props: {
+  },
   data: () => ({
     step: 1
   }),
-  props: {
-    source: String
+  setup() {
+    const store = useStore()
+
+    const state = reactive({
+      form: {
+        id: '',
+        password: '',
+        nick: '',
+        name: '',
+        phone: '',
+        email: '',
+        userimage: '',
+      },
+    })
+
+    onMounted(()=>{
+    })
+
+    const clickLogin = function () {
+      loginForm.value.validate((valid)=>{
+        if(valid){
+          store.dispatch('root/requestLogin',{
+            userId: state.form.id,
+            userPwd: state.form.password
+          }).then(function (result){
+            swal("Success!","로그인 성공", "success")
+            localStorage.setItem('jwt',result.data.accessToken)
+            location.reload()
+          })
+          .catch(function () {
+            swal('Fail','아이디와 비밀번호를 확인해주세요','error')
+          })
+        }
+      })
+    }
+
+    return { state, clickLogin}
   }
 };
 </script>
@@ -157,4 +243,10 @@ export default {
     background-repeat : no-repeat;
     background-size : cover;
   }
+.elevation-12 {
+  background-color: rgba( 255, 255, 255, 0.0 );
+}
+.back-opt {
+  background-color: rgba( 255, 255, 255, 0.5 );
+}
 </style>
