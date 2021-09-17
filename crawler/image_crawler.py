c99a6c47d0aae1f6685aa79a62ef82d66489b896
Jihoon Byun <wlgns1588@naver.com>
2021-09-17 15:11:42 +0900

    chore: 이미지 설명도 함께 크롤링

--- crawler/image_crawler.py
+++ crawler/image_crawler.py
@@ -10,9 +10,9 @@ import googletrans
 def image_crawling(keyword):
     driver = driver_init()
     # 한글 -> 영어 번역
-    keyword = googletrans.Translator().translate(keyword, dest='en').text
+    eng_keyword = googletrans.Translator().translate(keyword, dest='en').text
 
-    url = 'https://unsplash.com/s/photos/' + keyword
+    url = 'https://unsplash.com/s/photos/' + eng_keyword
     # 페이지 로드를 위해 3초 대기
     driver.implicitly_wait(3)
     driver.get(url)
@@ -24,8 +24,10 @@ def image_crawling(keyword):
 
     images = driver.find_elements_by_class_name("oCCRx")
     n = 1
+    img_list = []
     for image in images:
         img_urls = image.get_attribute('srcset')
+        img_desc = image.get_attribute('alt')
 
         if img_urls is None:
             continue
@@ -36,27 +38,29 @@ def image_crawling(keyword):
             continue
 
         img_url = img_urls[20]
-
+        temp = [img_desc]
+        img_list.append(temp)
         with urlopen(img_url) as f:
-            with open('./images/' + keyword + str(n) + '.jpg', 'wb') as h:  # 이미지 + 사진번호 .jpg
+            with open('./crawler/images/' + keyword + str(n) + '.jpg', 'wb') as h:  # 이미지 + 사진번호 .jpg
                 img = f.read()  # 이미지 읽기
                 h.write(img)
         n += 1
-        if n > 20:
+        if n > 5:
             break
+    print(img_list)
 
 
 def driver_init():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(
-        chrome_options=options,  executable_path=r'C:/SSAFY/2학기/특화PJT/crawler/chromedriver.exe')
+        chrome_options=options,  executable_path=r'C:/SSAFY/2학기/특화PJT/특화PJT/crawler/chromedriver.exe')
 
     return driver
 
 
 def main():
-    image_crawling("더욱")
+    image_crawling("엄마")
 
 
 if __name__ == '__main__':
