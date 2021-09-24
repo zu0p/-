c55e872e7b98ca458d83133abb1c41b9ef0c6537
Jihoon Byun <wlgns1588@naver.com>
2021-09-24 12:02:53 +0900

    chore: 초기 데이터 크롤링용

--- crawler/image_crawler.py
+++ crawler/image_crawler.py
@@ -1,10 +1,12 @@
-import pandas as pd
-from bs4 import BeautifulSoup
 import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from urllib.request import urlopen
 import googletrans
+import pymysql
+
+db = None
 
 
 def image_crawling(keyword):
@@ -24,8 +26,11 @@ def image_crawling(keyword):
 
     images = driver.find_elements_by_class_name("oCCRx")
     n = 1
+    img_descs = []
+    img_save_urls = []
     for image in images:
         img_urls = image.get_attribute('srcset')
+        img_desc = image.get_attribute('alt')
 
         if img_urls is None:
             continue
@@ -36,27 +41,64 @@ def image_crawling(keyword):
             continue
 
         img_url = img_urls[20]
+        img_descs.append(img_desc)
 
         with urlopen(img_url) as f:
-            with open('./images/' + keyword + str(n) + '.jpg', 'wb') as h:  # 이미지 + 사진번호 .jpg
+            with open('../images/keywords/' + keyword + str(n) + '.jpg', 'wb') as h:  # 이미지 + 사진번호 .jpg
                 img = f.read()  # 이미지 읽기
                 h.write(img)
+        img_save_urls.append(
+            '/home/ubuntu/images/keywords/' + keyword + str(n) + '.jpg')
         n += 1
         if n > 20:
             break
+
+    driver.close()
+    return keyword, img_save_urls, img_descs
 
 
 def driver_init():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(
-        chrome_options=options,  executable_path=r'C:/SSAFY/2학기/특화PJT/crawler/chromedriver.exe')
+        chrome_options=options,  executable_path=r'../chromedriver')
 
     return driver
 
 
 def main():
-    image_crawling("더욱")
+    global db
+
+    db = pymysql.connect(
+        user='test',
+        passwd='test',
+        host='13.124.43.16',
+        db='ssafy_django_web',
+        charset='utf8'
+    )
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+
+    df = pd.read_excel(
+        './keywords(초기850개).xlsx', engine='openpyxl')
+    keywords = df['키워드']
+
+    cnt = 0
+    for word in keywords:
+        cnt += 1
+
+        if (cnt > 5):
+            break
+
+        key, img_urls, img_descs = image_crawling(word)
+        for i in range(len(img_urls)):
+            sql = "INSERT INTO `image` VALUES (%s, %s, %s, %s);"
+            val = (0, key, img_descs[i], img_urls[i])
+            # val = [['id', 0], ['keyword', key], [
+            #     'desc', img_descs[i]], ['url', img_urls[i]]]
+
+            cursor.execute(sql, val)
+
+            db.commit()
 
 
 if __name__ == '__main__':
