aa74e21d4719146a4124ec9f69a8865326c84edb
변지훈 <wlgns1588@naver.com>
2021-10-01 16:53:19 +0900

    chore: 회정정보 조회 수정중

--- frontend/src/components/main/UserMenu.vue
+++ frontend/src/components/main/UserMenu.vue
@@ -2,10 +2,15 @@
   <v-layout id="user-menu">
     <v-flex xs12 sm12 md12>
       <div class="text-right">
-        <v-btn dark class="btn" text @click="clickUserInfo()">
-          <v-icon dark left>mdi-account-circle-outline</v-icon>
-          프로필
-        </v-btn>
+        <v-dialog v-model="dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark class="btn" v-bind="attrs" v-on="on" text>
+              <v-icon dark left>mdi-account-circle-outline</v-icon>
+              프로필
+            </v-btn>
+          </template>
+          <user-profile :isAdd="true" @closeUserInfoDialog="onCloseUserInfoDialog" />
+        </v-dialog>
         <v-btn dark class="btn" text @click="clickLogout()">
           <v-icon dark left>mdi-close-thick </v-icon>
           로그아웃
@@ -17,11 +22,16 @@
 
 <script>
 import { mapActions } from "vuex";
+import UserProfile from "./UserProfile.vue";
 const userStore = "userStore";
 export default {
   name: "UserMenu",
+  components: {
+    UserProfile,
+  },
   data() {
     return {
+      dialog: false,
       userInfo: {
         userEmail: "",
         userId: "",
@@ -29,7 +39,6 @@ export default {
         userName: "",
         userNick: "",
         userPhone: "",
-        userPwd: "",
       },
     };
   },
@@ -55,6 +64,9 @@ export default {
       this.logout();
       window.localStorage.removeItem("access-token");
       window.location = "/";
+    },
+    onCloseUserInfoDialog() {
+      this.dialog = false;
     },
   },
 };
