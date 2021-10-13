be8f70c7b5f3cf2771720dee1d7594e207349d41
변지훈 <wlgns1588@naver.com>
2021-10-13 13:45:21 +0900

    db 설정 변경

---
+++ dataServer/dataServer/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ssafy_django_web',  # DB name
+        'USER': 'test',  # DB account
+        'PASSWORD': 'test',  # DB account's password
+        'HOST': '3.35.37.110',  # DB address(IP)
+        'PORT': '3306',  # DB port(normally 3306)
+    }
+}
+
+SECRET_KEY = 'django-insecure-0yg7q35%x4tl#ncxy9z-9k3q^xl=7xfx4$slji98(h5=gf^y)n'
